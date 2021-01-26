e26f9d79b9222d94830a9495673d3914d8c4d628
Benis Tambe <will.jenifer@yahoo.com>
2021-01-25 22:42:07 -0500

    commit tree 3f31cc3d50a9454698a50f3b76e6d6f5df8cbadd

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,11 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition, :cobble
+    def initialize(shoe_brand)
+        @brand = shoe_brand
+    end
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
